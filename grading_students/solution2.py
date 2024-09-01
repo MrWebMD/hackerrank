320f1256757ddb0e75ea354c27fdfb9879f50d29
MrWebMD <mrwebmd@protonmail.com>
2024-09-01 16:49:01 -0400

    Grading Students

---
+++ grading_students/solution2.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+
+def gradingStudents(grades):
+    # Write your code here
+
+    output = []
+
+    for grade in grades:
+        if grade >= 38:
+            closest_multiple = (grade - grade % 5) + 5
+            if closest_multiple - grade < 3:
+                output.append(closest_multiple)
+                continue
+
+        output.append(grade)
+
+    return output
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
